1139c68405da10ab06175c4123644e1b7fdbc3fe
guzleek <guzl1k.hg@gmail.com>
2026-03-21 18:50:19 +0200

    add logging

---
+++ src/main/java/com/post_hub/iam_service/controller/PostController.java
@@ -0,0 +1,32 @@
+package com.post_hub.iam_service.controller;
+
+import com.post_hub.iam_service.model.constants.ApiLogMessage;
+import com.post_hub.iam_service.model.dto.post.PostDTO;
+import com.post_hub.iam_service.model.response.IamResponse;
+import com.post_hub.iam_service.service.PostService;
+import com.post_hub.iam_service.utils.ApiUtils;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/posts")
+public class PostController {
+
+    private final PostService postService;
+
+    @GetMapping("/{id}")
+    public ResponseEntity<IamResponse<PostDTO>> getPostById(
+            @PathVariable(name = "id") Integer postId) {
+        log.trace(ApiLogMessage.NAME_OF_CURRENT_METHOD.getValue(), ApiUtils.getMethodName());
+
+        IamResponse<PostDTO> response = postService.getById(postId);
+        return ResponseEntity.ok(response);
+    }
+}
---
+++ src/main/java/com/post_hub/iam_service/model/constants/ApiLogMessage.java
@@ -0,0 +1,15 @@
+package com.post_hub.iam_service.model.constants;
+
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+public enum ApiLogMessage {
+    POST_INFO_BY_ID("Receiving post with ID: {}"),
+    NAME_OF_CURRENT_METHOD("Current method: {}"),
+    ;
+    private final String value;
+
+}
